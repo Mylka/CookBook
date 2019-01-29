94b2fd35293c3f74374f12be4a22bf3ffb2523f9
Marylka <lmary155@gmail.com>
2019-01-29 11:04:11 +0000

    reset to initial depl

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ var express       = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index")
-    
 
 
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_v12";
+mongoose.connect(url, {useNewUrlParser: true});
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -45,7 +47,7 @@ app.use(flash());
 
 //PASSPORT CONFIGURATION
 app.use(require("express-session")({
-    secret:"noideawhatamdoin",
+    secret:"dot pig",
     resave: false,
     saveUninitialized: false
 }));
@@ -73,5 +75,5 @@ app.use(commentRoutes);
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("the yelpCamp Server Has Started!!!");
+    console.log("Server Has Started!!!");
 })
